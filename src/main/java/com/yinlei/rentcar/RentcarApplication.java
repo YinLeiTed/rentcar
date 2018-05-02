ccb7bb53ad0d4020ef5df896bd4689c6a8b5153d
ted <yin_lei@outlook.com>
2018-05-02 22:15:39 +0800

    1.完成图片上传，本地图片浏览预览
    Signed-off-by: ted <yin_lei@outlook.com>

---
+++ src/main/java/com/yinlei/rentcar/RentcarApplication.java
@@ -0,0 +1,12 @@
+package com.yinlei.rentcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentcarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentcarApplication.class, args);
+    }
+}
---
+++ src/main/resources/static/js/main2.js
@@ -0,0 +1,6 @@
+document.write("<link rel='stylesheet' href='./css/bootstrap.min.css' />")
+document.write("<link rel='stylesheet' href='./layui/css/layui.css' />")
+document.write("<script language='javascript' src='./js/jquery-3.3.1.min.js'></script>");
+document.write("<script language='javascript' src='./js/jquery.cookie.js'></script>");
+document.write("<script language='javascript' src='./js/bootstrap.min.js'></script>");
+document.write("<script language='javascript' src='./layui/layui.js'></script>");
